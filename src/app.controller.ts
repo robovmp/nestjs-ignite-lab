f2ba2b249901100f9ec168b6e4d9ddeb73369182
robovmp <mpviniciusmp@gmail.com>
2023-01-03 09:38:53 -0300

    config inicial

---
+++ src/app.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { randomUUID } from 'crypto';
+import { CreateNotificationBody } from './create-notification-cody';
+
+import { PrismaService } from './prisma.service';
+
+@Controller('notifications')
+export class AppController {
+  constructor(private readonly prisma: PrismaService) {}
+
+  @Get()
+  list() {
+    return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async create(@Body() body: CreateNotificationBody) {
+    const { category, recipientId, content } = body;
+
+    await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content,
+        category,
+        recipientId,
+      },
+    });
+  }
+}
